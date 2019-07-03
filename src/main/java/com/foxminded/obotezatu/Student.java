fa4f07cbc9856a06e3f5a434850d185daec0deb3
md_obotezatu <md_obotezatu@MD2CFDA16E7FDF.alliedtesting.com>
2019-07-03 20:47:28 +0300

    draft version 2

---
+++ src/main/java/com/foxminded/obotezatu/Student.java
@@ -0,0 +1,42 @@
+package com.foxminded.obotezatu;
+
+public class Student {
+
+	private String studentId;
+	private String groupId;
+	private String firstName;
+	private String lastName;
+
+	public String getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getGroupId() {
+		return groupId;
+	}
+
+	public void setGroupId(String groupId) {
+		this.groupId = groupId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
---
+++ src/main/java/com/foxminded/obotezatu/StudentDao.java
@@ -0,0 +1,7 @@
+package com.foxminded.obotezatu;
+
+public class StudentDao extends Dao<Student> {
+	
+	
+
+}
