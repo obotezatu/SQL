14b02f022956148ae453311b3d8f8b2a15802779
Oleg Botezatu <md_obotezatu@alliedtesting.com>
2019-07-12 15:09:09 +0300

    draft version 13

--- query.sql
+++ query.sql
@@ -1,8 +1,8 @@
 --SQL query should find if any of the groups has less than 10 students.
 
-SELECT  groups.name, COUNT (students.group_id)
+	SELECT  groups.name, COUNT (students.group_id) AS count
 	FROM students
-	INNER JOIN groups
+	RIGHT JOIN groups
 	ON students.group_id = groups.group_id
 	GROUP BY groups.name
 	HAVING
@@ -12,11 +12,11 @@ SELECT  groups.name, COUNT (students.group_id)
 
 DELETE FROM students 
 	USING groups
-	WHERE students.group_id = groups.group_id AND groups.name = 1;
+	WHERE students.group_id = groups.group_id AND groups.name = 'SR-01';
 	
 --SQL query that finds name of course and related students.
 	
-SELECT students.first_name, students.last_name, groups.name as group, courses.name as course
+SELECT courses.name as course, students.last_name, students.first_name
 	FROM students
 	INNER JOIN groups
 	ON students.group_id = groups.group_id
@@ -24,5 +24,5 @@ SELECT students.first_name, students.last_name, groups.name as group, courses.na
 	ON students.student_id = cs.student_id
 	INNER JOIN courses 
 	ON courses.course_id = cs.course_id
-	WHERE courses.name LIKE 'Java Fun%';
+	ORDER BY courses.name, students.last_name;
 	
